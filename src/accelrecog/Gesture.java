c054bb23218d9434adff4009cb401844ec7ebad6
Jamesdu29 <dspreadt-h@gmx.com>
2019-06-03 14:45:18 +0200

    Adding Julien's part

--- src/accelrecog/Gesture.java
+++ src/accelrecog/Gesture.java
@@ -1,36 +1,24 @@
 package accelrecog;
 
 import accelrecog.globalListener_actor.GlobalListener;
-import org.opencv.core.Mat;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Gesture {
+
     public LinkedList<DataSet> mySets = new LinkedList<>();
     public String myName;
     public Shortcut myShortCut;
+
     public Gesture(ArrayList<Data> data, String n, GlobalListener globalListener){
         myName = n;
         mySets.add(new DataSet(data,n+"0"));
         myShortCut = new Shortcut(globalListener);
     }
-    public Gesture(LinkedList<DataSet> data, String myName, Shortcut shortcut){
+    public Gesture(LinkedList<DataSet> sets, String myName, Shortcut shortcut){
+        mySets = sets;
         this.myName = myName;
-        mySets = data;
         myShortCut = shortcut;
-    }
-
-    public Gesture(){
-        myName ="";
-        ArrayList<Data> data = new ArrayList<>();
-        for(int i = 0;i< Math.random()*10000;i++){
-            data.add(new Data(Math.random(),Math.random(),Math.random(),(byte)(Math.random()*2),(byte)(Math.random()*2),(byte)(Math.random()*2)));
-        }
-        mySets.add(new DataSet(data,"0"));
-    }
-    public Gesture(LinkedList<DataSet> a, String b){
-
     }
 
     public void reinforce(ArrayList<Data> data){
